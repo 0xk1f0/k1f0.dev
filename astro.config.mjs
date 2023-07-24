0e39fc3cf69719f895127747090aec5b39a12478
0xk1f0 <fteppan@protonmail.com>
2023-07-24 21:43:00 +0200

    add new transition integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
 import { defineConfig } from 'astro/config';
 import deno from '@astrojs/deno';
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: deno(),
-  publicDir: './src/public'
+  publicDir: './src/public',
+  experimental: {
+    viewTransitions: true
+  },
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'github-dark-dimmed',
+      langs: [],
+      wrap: true,
+    },
+  },
+  integrations: []
 });
