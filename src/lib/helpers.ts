f278f18cb642c18ab8dfdfc6b4c845430f0e0109
0xk1f0 <dev@k1f0.dev>
2024-04-20 20:38:57 +0200

    fix(fetch): make post fetching less taxing

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,16 +1,27 @@
-import matter from 'gray-matter';
-import type { GrayMatterFile } from 'gray-matter';
-import { promises as fs } from 'fs';
-import { extname, join } from 'path';
+import matter from "gray-matter";
+import type { GrayMatterFile } from "gray-matter";
+import { promises as fs } from "fs";
+import { extname, join } from "path";
 
-export async function getRawPostData(path: string) {
+export async function getRawPostList(path: string) {
     const DIR = await fs.readdir(path);
-    const FILES = DIR.filter(file => extname(file) === '.md');
+    const FILES = DIR.filter((file) => extname(file) === ".md");
     let rawPostsData: GrayMatterFile<string>[] = [];
     for (const file of FILES) {
         const filePath = join(path, file);
-        const content = await fs.readFile(filePath, 'utf8');
+        const content = await fs.readFile(filePath, "utf8");
         rawPostsData.push(matter(content));
     }
     return rawPostsData;
+}
+
+export async function getRawPostData(path: string, name: string) {
+    const DIR = await fs.readdir(path);
+    const FILE = DIR.find(
+        (file) => file.endsWith(name + ".md")
+    );
+    if (FILE === undefined) return undefined;
+    const filePath = join(path, FILE);
+    const content = await fs.readFile(filePath, "utf8");
+    return matter(content);
 }
