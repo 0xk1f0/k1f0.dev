f30dd9563d7f58e9ee6e246d43564d5969e5a7f5
0xk1f0 <dev@k1f0.dev>
2024-09-28 20:39:37 +0200

    fix(css): proper codeblock wrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
-  publicDir: "./src/public",
-  site: 'https://blog.k1f0.dev',
-  integrations: [tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  experimental: {
-    serverIslands: true,
-  },
-  prefetch: {
-    defaultStrategy: 'viewport'
-  }
+    publicDir: "./src/public",
+    site: "https://blog.k1f0.dev",
+    integrations: [tailwind()],
+    output: "server",
+    adapter: node({
+        mode: "standalone",
+    }),
+    experimental: {
+        serverIslands: true,
+    },
+    prefetch: {
+        defaultStrategy: "viewport",
+    },
 });
