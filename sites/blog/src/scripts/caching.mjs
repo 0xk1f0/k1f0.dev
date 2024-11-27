cb1ff1d8081b469d9a31efcd32ee3313215207ad
0xk1f0 <dev@k1f0.dev>
2024-11-27 21:21:19 +0100

    chore: move to Containerfiles and cleanup image generation

--- sites/blog/src/scripts/caching.mjs
+++ sites/blog/src/scripts/caching.mjs
@@ -1,9 +1,10 @@
 import { createClient } from "@redis/client";
 import matter from "gray-matter";
+import path from "path";
 import { promises as fs } from "fs";
 import { extname, join } from "path";
 
-const POSTS_PATH = process.env.POSTS_PATH || "/posts";
+const POSTS = path.normalize(process.env.POSTS || "/posts");
 const DB_HOST = process.env.DB_HOST || "localhost";
 const CLIENT = createClient({
     url: `redis://${DB_HOST}:6379`,
@@ -13,10 +14,10 @@ await CLIENT.connect();
 try {
     // iterate all posts and save to db
     await CLIENT.flushDb();
-    const DIR = await fs.readdir(POSTS_PATH);
+    const DIR = await fs.readdir(POSTS);
     const FILES = DIR.filter((file) => extname(file) === ".md");
     for (const file of FILES) {
-        const filePath = join(POSTS_PATH, file);
+        const filePath = join(POSTS, file);
         const content = await fs.readFile(filePath, "utf8");
         const CONTENT = matter(content);
         await CLIENT.set(`${CONTENT.data.shortcut}`, JSON.stringify(CONTENT));
